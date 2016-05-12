ced7229c2e807fcb312030b132a2ba5c2d02ba12
Daniel Stewart <daniel.robbins.stewart@gmail.com>
2016-05-12 13:23:49 -0600

    Finished /api/v1/users/signup

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
-var express = require('express');
-var router = express.Router();
-var knex = require('../db')
+const express = require('express');
+const router = express.Router();
+const knex = require('../db')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken');
 
 /* GET users listing. */
 router.post('/signup', function(req, res, next) {
@@ -22,7 +24,28 @@ router.post('/signup', function(req, res, next) {
     .then(function (result) {
       // {count: "0"}
       if (result.count === "0") {
-        // We're good
+        const saltRounds = 4;
+        const passwordHash = bcrypt.hashSync(req.body.password, saltRounds);
+
+        knex('users')
+        .insert({
+          email: req.body.email,
+          name: req.body.name,
+          password_hash: passwordHash
+        })
+        .returning('*')
+        .then(function (users) {
+          const user = users[0];
+          const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET);
+
+          res.json({
+            id: user.id,
+            email: user.email,
+            name: user.name,
+            token: token
+          })
+        })
+
       } else {
         res.status(422).json({
           errors: ["Email has already been taken"]
