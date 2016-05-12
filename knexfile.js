d839f166c94d274e35d9d5e642164d7b122b3837
Daniel Stewart <daniel.robbins.stewart@gmail.com>
2016-05-12 10:41:53 -0600

    Add migration for users table

---
+++ knexfile.js
@@ -0,0 +1,18 @@
+// Update with your config settings.
+
+require('dotenv').config();
+
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL
+
+  },
+
+};
